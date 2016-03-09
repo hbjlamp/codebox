4717b59eb2b116915da146f59a2716ffdedf5804
huxiaojun <595736620@qq.com>
2016-03-09 22:46:14 +0800

    add pianduan.js

---
+++ pianduan.js
@@ -0,0 +1,27 @@
+/*url地址处理*/
+function isExitsVariable(variableName) {
+    try {
+        if (typeof(variableName) == "undefined" || variableName=='') {
+            return false;
+        } else {
+            return true;
+        }
+    } catch(e) {}
+    return false;
+}
+
+var $_GET = (function(){
+    var url = window.document.location.href.toString();
+    var u = url.split("?");
+    if(typeof(u[1]) == "string"){
+        u = u[1].split("&");
+        var get = {};
+        for(var i in u){
+            var j = u[i].split("=");
+            get[j[0]] = j[1];
+        }
+        return get;
+    } else {
+        return {};
+    }
+})();
